a541e8ed3255bfc482073d8b3078cc0937bd20d3
dmi3coder <dmi3coder@gmail.com>
2017-09-09 21:09:50 +0300

    Initial Commit

---
+++ flat/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  flat
+//
+//  Created by dmi3coder on 9/9/17.
+//  Copyright © 2017 io.github.dmi3coder. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ flatTests/flatTests.swift
@@ -0,0 +1,36 @@
+//
+//  flatTests.swift
+//  flatTests
+//
+//  Created by dmi3coder on 9/9/17.
+//  Copyright © 2017 io.github.dmi3coder. All rights reserved.
+//
+
+import XCTest
+@testable import flat
+
+class flatTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
